a3475df7c6dcef4143cf94924c640e9b5af184f9
Mauro Henrique Brandão dos Santos <mhhbssantos@gmail.com>
2024-03-25 18:12:22 -0300

    Programando na USP

---
+++ ExerciciosGEMA/Raizes.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main(){
+  float n, r;
+  cin >> n;
+  for (int i =0; i<n;i++){
+    cin >> r;
+    sqrt(r);
+  }
+  return (0);
+
+}
